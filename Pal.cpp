7e75705cd3449a56afecd544fcfb8a933a9973ec
Gregg H <50878128+GreggZumbari@users.noreply.github.com>
2019-09-20 10:49:35 -0700

    End of 9/20/2019

--- Pal.cpp
+++ Pal.cpp
@@ -6,15 +6,35 @@ int main() {
 
   bool pal = true;
   int lim = 80;
-  char input[lim + 1];
+  char input[lim];
 
-  //Get input
-  cin.get(input, lim + 1);
-
-  //Note to self: delete punctuation
+  //Clear RAM data
+  for (int i = 0; i <= sizeof(input); i++)
+    input[i] = 32;
   
-  for (int i = 0; i < lim + 1; i++) {
-    cout << input[i] << endl;
+  //Get input
+  cin.getline(input, lim);
+
+  //Convert to lowercase and then destroy all other data
+  for (int i = 0; i <= sizeof(input); i++) {
+    //Convert to lowercase
+    if (input[i] >= 65 && input[i] <= 90)
+      input[i] += 32;
+    //Expel non-letters
+    if (input[i] < 97 || input[i] > 122)
+      input[i] = (char)32;
+    //Expel spacing (very inefficiently)
+    if (input[i] == 32) {
+      if (input[i + 1] == 32) {
+	for (int j = i; i < sizeof(input) - i; j++) {
+	  //If the next space is also a space, keep looking until it isnt
+	}
+      }
+    }
+  }
+  
+  for (int i = 0; i < sizeof(input); i++) {
+    cout << input[i] << ", ";
   }
 
 }
