1433ce6d0f78f08b4720cf0d75bb06dc2cea91e6
GreggH <greggory.png@gmail.com>
2019-09-30 16:54:42 -0700

    version 1.0
    This is the completed program

--- Pal.cpp
+++ Pal.cpp
@@ -14,13 +14,15 @@ int main() {
   bool pal = true;
 
   //Max char limit
-  int lim = 80;
+  int lim = 81;
   //Offset between input and pInput in a given array
   int offs = 0;
+  //CString length because strlen() doesn't work
+  int pLen = 0;
 
-  //User input (it's really big in the hopes that nobody will type in anything higher)
-  char input[2000];
-  //Parsed user input, will only take the first 80 characters
+  //User input
+  char input[lim];
+  //Parsed user input
   char pInput[lim];
 
   //Clear RAM data
@@ -31,37 +33,45 @@ int main() {
   
   //Get input
   cout << "Greetings traveller! You have happened upon your local Whole Foods." << endl
-       << "Enter a phrase and our trademark skinny white boy will tell you if it\'s a palindrome." << endl;
-  cin.getline(input, 2000);
+       << "Enter a phrase and our trademark skinny white boy will tell you if it\'s a palindrome." << endl
+       << "Phrase: ";
+  cin.getline(input, lim);
+
+  /*
+  for (int i = 0; i < lim; i++) {
+    cout << input[i];
+  }
+  cout << endl;
+  */
 
   //Convert to lowercase and then destroy all other data
   for (int i = 0; i <= sizeof(input); i++) {
     //Convert to lowercase
     if (input[i] >= 65 && input[i] <= 90)
-      input[i] += 32;
+      input[i] += (char)32;
     //Expel non-letters
-    if (input[i] < 97 && input[i] > 122)
+    if (input[i] < 97 || input[i] > 122)
       input[i] = (char)32;
     //Expel spaces (by moving all of the data over to another char[])
-    if (input[i] != 32)
+    if (input[i] != 32) {
       pInput[i - offs] = input[i];
+      pLen++;
+    }
     else offs++;
   }
 
-  cout << strlen(pInput) << endl;
-
   //Test for palindrome, finally
-  for (int i = 0; i < strlen(pInput); i++) {
-    cout << (int)pInput[i] << ", " << (int)pInput[strlen(pInput)] << endl;
-    if ((int)pInput[i] != (int)pInput[strlen(pInput) - i])
+  for (int i = 0; i < (pLen / 2) + 1; i++) {
+    /*//Uncomment to see the process through which the program tests if the phrase is a palindrome
+      cout << (int)pInput[i] << "(" << (char)pInput[i] << "), " << (int)pInput[pLen - 1 - i] << "(" << (char)pInput[pLen - 1 - i] << ")" << endl;
+    */
+    if ((int)pInput[i] != (int)pInput[pLen - 1 - i])
       pal = false;
-    else
-      cout << "Same" << endl;
   }
 
   //Return the results!
-  if (pal == false)
-    cout << "This phrase is not a palindrome." << endl;
+  if (pal == true)
+    cout << "Palindrome." << endl;
   else
-    cout << "This phrase indeed is a palindrome." << endl;
+    cout << "Not a palindrome." << endl;
 }
